8a0905423dfcb30708c0cb3da96f2b6ac61c80c4
Richard Widjaya <ricwidjaya@gmail.com>
2022-01-07 17:27:02 +0800

    config: add routes

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ if (process.env.NODE_ENV !== "production") {
 }
 const PORT = process.env.PORT
 const app = express()
+const routes = require("./routes")
 const exphbs = require("express-handlebars")
 
 // View engine config
 app.engine("hbs", exphbs.engine({ extname: ".hbs" }))
 app.set("view engine", "hbs")
 
-app.get("/", (req, res) => {
-  res.render("index")
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App server host on ${PORT}`)
