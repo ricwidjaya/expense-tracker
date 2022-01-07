ce9d14210a82293fdf38280038ec59a8baee47c0
Richard Widjaya <ricwidjaya@gmail.com>
2022-01-07 17:21:35 +0800

    feat: add handlebars view engine & bootstrap v5.1

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ if (process.env.NODE_ENV !== "production") {
 }
 const PORT = process.env.PORT
 const app = express()
+const exphbs = require("express-handlebars")
+
+// View engine config
+app.engine("hbs", exphbs.engine({ extname: ".hbs" }))
+app.set("view engine", "hbs")
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.render("index")
 })
 
 app.listen(PORT, () => {
