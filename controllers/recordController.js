e7cf54f830f0845f810e66c9fc3f5c063d8d4525
Richard Widjaya <ricwidjaya@gmail.com>
2022-01-08 19:23:17 +0800

    feat: add new record page

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -1,5 +1,18 @@
 module.exports = {
+  // Render index page
   getRecords: (req, res) => {
     return res.render("index")
+  },
+
+  // New record page
+  newRecord: (req, res) => {
+    return res.render("new", {
+      style: "record"
+    })
+  },
+
+  // Add new record
+  postRecord: (req, res) => {
+
   }
 }
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,5 +4,10 @@ const router = express.Router()
 const recordController = require("../../controllers/recordController")
 
 router.get("/", recordController.getRecords)
+router.get("/record/new", recordController.newRecord)
+router.post("/record")
+router.get("/record/:id/edit")
+router.put("/record/:id")
+router.delete("/record/:id")
 
 module.exports = router
