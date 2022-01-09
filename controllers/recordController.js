bb2bf4514e165b5bd05a4c5293590c40497f9ff3
Richard Widjaya <ricwidjaya@gmail.com>
2022-01-09 17:15:36 +0800

    feat: complete index page

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -1,11 +1,18 @@
-
+const Category = require("../models/category")
+const Record = require("../models/record")
 
 module.exports = {
   // Render index page
   getRecords: (req, res) => {
+    const queries = [Category.find().lean(), Record.find().lean()]
 
-    return res.render("index", {
-      style: "index"
+    return Promise.all(queries).then(([categories, records]) => {
+      console.log(categories, records)
+      return res.render("index", {
+        categories,
+        records,
+        style: "index"
+      })
     })
   },
 
