85764d0f179c99dc4b599d48971d785f415c92b3
Richard Widjaya <ricwidjaya@gmail.com>
2022-01-10 17:15:56 +0800

    feat: add receipt to index AJAX script

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -48,6 +48,28 @@ function renderLists(data) {
               ${record.date}
             </div>
           </div>
+
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${record.name}Modal">
+  收據
+</button>
+
+<div class="modal fade" id="${record.name}Modal" tabindex="-1" aria-labelledby="${record.name}ModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="${record.name}ModalLabel">收據</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img src="${record.receipt}" alt="" width="300px" height="auto">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
           <div class="balance">
             $${record.formatAmount}
             元
